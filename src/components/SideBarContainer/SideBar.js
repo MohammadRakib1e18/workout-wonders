e0cb0d2879e4fcf3dfe4cc4ddcb6b16b0b0012a0
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-09-29 00:27:57 +0600

    added break time

--- src/components/SideBarContainer/SideBar.js
+++ src/components/SideBarContainer/SideBar.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SideBar.css';
 
 const SideBar = ({duration}) => {
-    return (
-        <div>
-            <section className="exercise-details">
-                <h2 className="heading">Exercise Details</h2>
-                <h4 className="duration"><span>Exercise time</span><span className="gray">{duration} seconds</span></h4>
+    const [breakTime, setBreakTime] = useState(0);
 
-            </section>
-        </div>
+    const breakHandler = (time) => {
+        setBreakTime(time);
+    }
+    return (
+      <div>
+        <section class="add-break">
+          <h2 class="heading">Add A Break</h2>
+          <ul className="break-options">
+            <li onClick={() => {breakHandler(10)}}>10s</li>
+            <li onClick={() => {breakHandler(20)}}>20s</li>
+            <li onClick={() => {breakHandler(30)}}>30s</li>
+            <li onClick={() => {breakHandler(40)}}>40s</li>
+            <li onClick={() => {breakHandler(50)}}>50s</li>
+          </ul>
+        </section>
+        <section className="exercise-details">
+          <h2 className="heading">Exercise Details</h2>
+          <h4 className="duration">
+            <span>Exercise time</span>
+            <span className="gray">{duration} seconds</span>
+          </h4>
+          <h4 className="duration">
+            <span>Break time</span>
+            <span className="gray">{breakTime} seconds</span>
+          </h4>
+        </section>
+      </div>
     );
 };
 
